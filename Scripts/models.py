1d9886dc83e6e743677871651db5107d1db34e00
mpreichenbach <matthew.reichenbach@gmail.com>
2021-05-25 08:35:05 -0600

    Removed UNet class

--- Scripts/models.py
+++ Scripts/models.py
@@ -7,72 +7,71 @@ from tensorflow.keras.layers import Activation, Add, BatchNormalization, Concate
 # UNet
 # ----------------------------------------------------
 
-class Unet:
-    def __init__(self,im_dim, df, depth=3):
-        self.im_dim = im_dim
-        self.df = df
-        self.classes = df['name'].tolist()
-        self.rgb_values = df['r', 'g', 'b'].tolist()
-        self.colors = df['color'].tolist()
-        self.color_dict = dict(zip(self.classes, self.colors))
-        self.depth = depth
+def main_block(m, n_filters, dim, bn, do_rate):
+    """The primary convolutional block in the UNet network.
+        Args:
 
-    def main_block(self, m, filters, bn, do_rate):
-        """The primary convolutional block in the UNet network"""
+            m (tensorflow layer): this is the previous layer in the network,
+            n_filters (int): number of filters in each convolution,
+            dim (int): the dimension of the filters,
+            bn (Boolean): whether to include batch normalization after each convolution,
+            do_rate (float): the rate to perform dropout before each convolution."""
 
-        n = Dropout(do_rate)(m)
-        n = Conv2D(filters, 3, activation='relu', padding='same')(n)
-        n = BatchNormalization()(n) if bn else n
-        n = Dropout(do_rate)(n) if do_rate else n
-        n = Conv2D(filters, (3, 3), activation='relu', padding='same')(n)
-        n = BatchNormalization()(n) if bn else n
-        return n
+    n = Dropout(do_rate)(m)
+    n = Conv2D(n_filters, dim, activation='relu', padding='same')(n)
+    n = BatchNormalization()(n) if bn else n
+    n = Dropout(do_rate)(n) if do_rate else n
+    n = Conv2D(n_filters, dim, activation='relu', padding='same')(n)
+    n = BatchNormalization()(n) if bn else n
+    return n
 
-    def model(self, filters, do_rate=0, bn=True, opt='Adam', loss='categorical_crossentropy'):
-        """Implements the UNet architecture, with options for Dropout and BatchNormalization
 
-            Args:
-                im_dim (int): this is the height or width of the inputs tiles (which should be square),
-                filters (int): the number of filters for the initial layer to have (which doubles each subsequent layer),
-                bn (Boolean): specifies weather to incorporate batch normalization,
-                do_rate (0 <= float <= 1): the value passed to the dropout layer,
-                opt (str): the optimizer for the model (see Keras documentation for options),
-                loss (str): the loss function for the model (should only change if creating a custom loss)."""
+def unet(im_dim, n_filters, dim=3, do_rate=0, bn=True, opt='Adam', loss='categorical_crossentropy'):
+    """Implements the UNet architecture, with options for Dropout and BatchNormalization
 
-        # Downsampling path
-        input_img = Input(shape=(self.im_dim, self.im_dim, self.depth))
+        Args:
+            im_dim (int): this is the height or width of the inputs tiles (which should be square),
+            n_filters (int): the number of filters for the initial layer to have (which doubles each subsequent layer),
+            dim (int): the dimension of the filters,
+            do_rate (0 <= float <= 1): the rate to perform dropout before each convolution,
+            bn (Boolean): whether to include batch normalization after each convolution,
+            opt (str): the optimizer for the model (see Keras documentation for options),
+            loss (str): the loss function for the model (should only change if creating a custom loss)."""
 
-        b1 = self.main_block(input_img, filters=filters, bn=bn, do_rate=do_rate)
-        max_1 = MaxPooling2D(pool_size=(2, 2), padding='same')(b1)
-        filters *= 2
-        b2 = self.main_block(max_1, filters=filters, bn=bn, do_rate=do_rate)
-        max_2 = MaxPooling2D(pool_size=(2, 2), padding='same')(b2)
-        filters *= 2
-        b3 = self.main_block(max_2, filters=filters, bn=bn, do_rate=do_rate)
-        max_3 = MaxPooling2D(pool_size=(2, 2), padding='same')(b3)
-        filters *= 2
-        b4 = self.main_block(max_3, filters=filters, bn=bn, do_rate=do_rate)
+    # Downsampling path
+    input_img = Input(shape=(im_dim, im_dim, 3))
 
-        # Upsampling path
-        up_1 = UpSampling2D(size=(2, 2))(b4)
-        con_1 = Concatenate(axis=-1)([up_1, b3])
-        filters = int(filters / 2)
-        b5 = self.main_block(con_1, filters=filters, bn=bn, do_rate=do_rate)
-        up_2 = UpSampling2D(size=(2, 2))(b5)
-        con_2 = Concatenate(axis=-1)([up_2, b2])
-        filters = int(filters / 2)
-        b6 = self.main_block(con_2, filters=filters, bn=bn, do_rate=do_rate)
-        up_3 = UpSampling2D(size=(2, 2))(b6)
-        con_3 = Concatenate(axis=-1)([up_3, b1])
-        filters = int(filters / 2)
-        b7 = self.main_block(con_3, filters=filters, bn=bn, do_rate=do_rate)
-        output_img = Conv2D(len(self.df), (1, 1), padding='same', activation='softmax')(b7)
+    b1 = main_block(input_img, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
+    max_1 = MaxPooling2D(pool_size=(2, 2), padding='same')(b1)
+    n_filters *= 2
+    b2 = main_block(max_1, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
+    max_2 = MaxPooling2D(pool_size=(2, 2), padding='same')(b2)
+    n_filters *= 2
+    b3 = main_block(max_2, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
+    max_3 = MaxPooling2D(pool_size=(2, 2), padding='same')(b3)
+    n_filters *= 2
+    b4 = main_block(max_3, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
 
-        # instantiates a model and compiles with the optimizer and loss function
-        cnn = Model(input_img, output_img)
-        cnn.compile(optimizer=opt, loss=loss)
+    # Upsampling path
+    up_1 = UpSampling2D(size=(2, 2))(b4)
+    con_1 = Concatenate(axis=-1)([up_1, b3])
+    n_filters = int(n_filters / 2)
+    b5 = main_block(con_1, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
+    up_2 = UpSampling2D(size=(2, 2))(b5)
+    con_2 = Concatenate(axis=-1)([up_2, b2])
+    n_filters = int(n_filters / 2)
+    b6 = main_block(con_2, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
+    up_3 = UpSampling2D(size=(2, 2))(b6)
+    con_3 = Concatenate(axis=-1)([up_3, b1])
+    n_filters = int(n_filters / 2)
+    b7 = main_block(con_3, n_filters=n_filters, dim=dim, bn=bn, do_rate=do_rate)
+    output_img = Conv2D(3, 1, padding='same', activation='softmax')(b7)
 
-        return cnn
+    # instantiates a model and compiles with the optimizer and loss function
+    cnn = Model(input_img, output_img)
+    cnn.compile(optimizer=opt, loss=loss)
+
+    return cnn
 
 
 # ----------------------------------------------------
