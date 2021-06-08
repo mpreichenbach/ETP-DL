af8b601f0cc711f40a00cc252c510a79b9152dbb
mpreichenbach <matthew.reichenbach@gmail.com>
2021-06-08 12:16:25 -0600

    Added ISPRS data class

--- Scripts/helper_functions.py
+++ Scripts/helper_functions.py
@@ -30,139 +30,31 @@ class DigitalGlobeDataset():
 
         return sats, masks, oh_encoded
 
+class ISPRS():
+    """ISPRS semantic segmentation datasets, including Potsdam and Vaihingen separately or combined."""
 
-def extract_tiles(data_path, n_tiles, tile_dim, seed=0, im_dim=2448, fnr=True):
-    """Generates numpy arrays to hold training data. The output `sats' contains the satellite tiles, and `masks'
-    contains the corresponding pixel labels. Note that this function guarantees that overlapping tiles are NOT
-    selected; this can take a long time if the one tries to extract too many large tiles. For small and medium-sized
-    initial datasets, it will be faster just to segment every image into appropriately sized tiles.
+    def __init__(self, loc):
+        self.loc = loc
+        assert loc in {'Potsdam', 'Vaihingen'}, 'Location must be one of \'Potsdam\', \'Vaihingen\'.'
 
-    Args:
-        data_path (str): the location of the training data; imagery and masks should be together,
-        n_tiles (int): the number of tiles to extract,
-        tile_dim (int): height of the tile to extract (tiles will be square),
-        seed (int): sets the random seed value
-        im_dim (int): height of the input image to extract a tile from (input is assumed to be square),
-        fnr (Boolean): perform a random flip and rotation of each tile."""
+    def load(self, dim, masks = True, ir=False):
+        data_path = 'Data/ISPRS/' + self.loc + '/Numpy Arrays/'
 
-    # generate a list of all file names in the training data, and subset to only the satellite files
-    file_names = os.listdir(data_path)
-    file_names = [x for x in file_names if x.endswith('jpg')]
+        self.data_path = data_path
+        self.dim = dim
 
-    # choose random images (with replacement) from which to extract tiles from
-    if seed:
-        random.seed(seed)
-
-    # initializes the arrays which will hold the extracted tiles
-    sats = np.zeros((0, tile_dim, tile_dim, 3))
-    masks = np.zeros((0, tile_dim, tile_dim, 3))
-
-    init_image = np.random.randint(0, len(file_names))
-    init_row = np.random.randint(0, im_dim - tile_dim)
-    init_col = np.random.randint(0, im_dim - tile_dim)
-    coordinates = np.asarray([init_image, init_row, init_col])
-    coordinates = coordinates.reshape([1, 3])
-
-    # creates a list of coordinates [image, row, col] where row, col are the coordinates of the upper-left corner of a
-    # tile. When used later, the logic in this while-loop ensures that no coordinates will yield overlapping tiles
-    while len(coordinates) < n_tiles:
-        image = np.random.randint(0, len(file_names))
-        row = np.random.randint(0, im_dim - tile_dim)
-        col = np.random.randint(0, im_dim - tile_dim)
-        if image in coordinates[:, 0]:
-            matches = tuple(np.where(coordinates[:, 0] == image))
-
-            r_lower_bound = coordinates[matches, 1] - tile_dim
-            r_upper_bound = coordinates[matches, 1] + tile_dim
-            c_lower_bound = coordinates[matches, 2] - tile_dim
-            c_upper_bound = coordinates[matches, 2] + tile_dim
-
-            cond_r_1 = [x < row for x in r_lower_bound[0]]
-            cond_r_2 = [row < x for x in r_upper_bound[0]]
-            cond_c_1 = [y < col for y in c_lower_bound[0]]
-            cond_c_2 = [col < y for y in c_upper_bound[0]]
-
-            cond_r = [x and y for x, y in zip(cond_r_1, cond_r_2)]
-            cond_c = [x and y for x, y in zip(cond_c_1, cond_c_2)]
-
-            intersect = [x and y for x, y in zip(cond_r, cond_c)]
-
-            if True in intersect:
-                continue
-            else:
-                coordinates = np.append(coordinates, np.asarray([image, row, col]).reshape([1, 3]), axis=0)
+        if ir:
+            sats = np.load(data_path + 'RGBIR_tiles_' + str(dim) + '.npy')
         else:
-            coordinates = np.append(coordinates, np.asarray([image, row, col]).reshape([1, 3]), axis=0)
+            sats = np.load(data_path + 'RGB_tiles_' + str(dim) + '.npy')
 
-    counter = 0
+        enc = np.load(data_path + 'Encoded_tiles_' + str(dim) + '.npy')
 
-    for i in range(n_tiles):
-        [image, row, col] = coordinates[i]
-
-        im_name = file_names[image]
-        num = im_name.partition('_')[0]
-        sat_name = num + '_sat.jpg'
-        mask_name = num + '_mask.png'
-
-        if fnr:
-            f = random.randint(0, 2)
-            r = random.randint(0, 3)
-
-            # Opens the full satellite image, performs a random flip and rotation, then extracts a tile
-            with Image.open(os.path.join(data_path, sat_name)) as im:
-                if f == 1:
-                    im = im.transpose(Image.FLIP_LEFT_RIGHT)
-                if f == 2:
-                    im = im.transpose(Image.FLIP_TOP_BOTTOM)
-                im = im.rotate(r * 90)
-
-                sat_arr = np.asarray(im, dtype=np.uint8)
-                sat_tile = sat_arr[row:(row + tile_dim), col:(col + tile_dim)]
-                sat_tile = sat_tile.reshape([1, tile_dim, tile_dim, 3])
-
-            # Opens the full mask image, performs a random flip and rotation, then extracts a tile
-            with Image.open(os.path.join(data_path, mask_name)) as im:
-                if f == 1:
-                    im = im.transpose(Image.FLIP_LEFT_RIGHT)
-                if f == 2:
-                    im = im.transpose(Image.FLIP_TOP_BOTTOM)
-                im = im.rotate(r * 90)
-                mask_arr = np.asarray(im, dtype=np.uint8)
-                mask_tile = mask_arr[row:(row + tile_dim), col:(col + tile_dim)]
-                mask_tile = mask_tile.reshape([1, tile_dim, tile_dim, 3])
-
-        sats = np.append(sats, sat_tile, axis=0)
-        masks = np.append(masks, mask_tile, axis=0)
-
-        counter += 1
-        if counter % 50 == 0:
-            print(counter)
-
-    sats = sats.astype(np.uint8)
-    masks = masks.astype(np.uint8)
-
-    return [sats, masks]
-
-
-def save_tiles(sats, masks, sat_path, mask_path):
-    """This function saves the tiled images extracted by extract_tiles() to sat/mask folders of your choice, as png
-    files."""
-
-    for i in range(len(sats)):
-        sat_arr = sats[i]
-        mask_arr = masks[i]
-
-        sat_arr *= 255
-        mask_arr *= 255
-
-        sat_arr = sat_arr.astype(np.uint8)
-        mask_arr = mask_arr.astype(np.uint8)
-
-        sat_im = Image.fromarray(sat_arr)
-        mask_im = Image.fromarray(mask_arr)
-
-        sat_im.save(sat_path + '/' + str(i) + '.png')
-        mask_im.save(mask_path + '/' + str(i) + '.png')
+        if masks:
+            masks = np.load(data_path + 'RGBIR_tiles_' + str(dim) + '.npy')
+            return [sats, masks, enc]
+        else:
+            return [sats, enc]
 
 
 def rgb_to_oh(rgb_array, class_df):
