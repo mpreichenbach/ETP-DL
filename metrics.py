f2139f96b0e10d7d33e7159db8bf274fd5e34830
mpreichenbach <matthew.reichenbach@gmail.com>
2021-09-20 10:40:28 -0600

    Updated iou() and dice()

--- metrics.py
+++ metrics.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 
 
-def iou_loss(y_true, y_pred):
+def iou(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the IoU-loss for each class,
-    # then averages over the classes to get an overall score.
-    # https://en.wikipedia.org/wiki/Jaccard_index
+    # and returns a vector of IoU scores. See https://en.wikipedia.org/wiki/Jaccard_index for more information.
+    # Note that I used Tensorflow math functions so that this may be incorporated into a Keras losses
 
     y_true = tf.cast(y_true, dtype=tf.float32)
     y_pred = tf.cast(y_pred, dtype=tf.float32)
@@ -14,20 +14,20 @@ def iou_loss(y_true, y_pred):
     union = tf.math.add(y_true, tf.math.add(y_pred, -intersection))
     u_totals = tf.reduce_sum(union, axis=(1, 2))
 
-    # Note that some references insist on adding 1 to the numerator and denominator, or an epsilon to the denominator,
-    # to avoid division by zero. This is important when the IoU is used to gauge accuracy of bounding box predictions in
+    # Note that some references insist on adding 1 (or small epsilon) to the numerator and denominator, to avoid
+    # division by zero. This is important when the IoU is used to gauge accuracy of bounding box predictions in
     # object-detection tasks, because it's possible that the true and predicted bounding boxes are both empty (i.e.,
-    # when the network predicts a true negative). But in semantic segmentation tasks this is unnecessary, since the
-    # every voxel in the ground-truth and predicted tensors has at least one nonzero entry.
-    cat_iou = i_totals / u_totals
-    avg_iou = tf.reduce_mean(cat_iou)
+    # when the network predicts a true negative). But in semantic segmentation tasks this is unnecessary, since every
+    # voxel in the ground-truth and predicted tensors has at least one nonzero entry.
 
-    return 1 - avg_iou
+    iou_vec = i_totals / u_totals
 
-def dice_loss(y_true, y_pred):
+    return iou_vec
+
+def dice(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the dice-loss for each class,
-    # then averages over the classes to get an overall score.
-    # https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+    # and returns a vector of dice scores. See https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient for
+    # more information. Note that I used Tensorflow math functions to that this may be incorporated into a Keras losses
 
     y_true = tf.cast(y_true, dtype=tf.float32)
     y_pred = tf.cast(y_pred, dtype=tf.float32)
@@ -36,10 +36,9 @@ def dice_loss(y_true, y_pred):
     denominator = tf.reduce_sum(y_true, axis=(1, 2)) + tf.reduce_sum(y_pred, axis=(1, 2))
 
     # As in iou() above, we don't need to enforce nonzero denominators
-    cat_dice = numerator / denominator
-    avg_dice = tf.reduce_mean(cat_dice)
+    dice_vec = numerator / denominator
 
-    return 1 - avg_dice
+    return dice_vec
 
 def total_acc(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the total accuracy of the
