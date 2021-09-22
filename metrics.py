8875722381bb8fe880d09137eaccc2b8ec3be8a6
mpreichenbach <matthew.reichenbach@gmail.com>
2021-09-22 11:40:59 -0600

    Updated total_acc()

--- metrics.py
+++ metrics.py
@@ -18,7 +18,7 @@ def iou(y_true, y_pred):
 def dice(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the dice-loss for each class,
     # and returns a vector of dice scores. See https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient for
-    # more information. Note that I used Tensorflow math functions to that this may be incorporated into a Keras losses
+    # more information.
 
     numerator = 2 * np.sum(np.multiply(y_true, y_pred), axis=(0, 1, 2))
     denominator = np.sum(y_true, axis=(0, 1, 2)) + np.sum(y_pred, axis=(0, 1, 2))
@@ -30,20 +30,18 @@ def dice(y_true, y_pred):
     return dice_vec
 
 def total_acc(y_true, y_pred):
-    # Given input tensors of shape (batch_size, height, width, n_classes), this computes the total accuracy of the
-    # model's prediction.
+    # Given input tensors of shape (batch_size, height, width, n_classes), this computes the percentage accuracy of the
+    # model's prediction for each class.
 
-    y_pred = tf.where(
-        tf.equal(tf.reduce_max(y_pred, axis=-1, keepdims=True), y_pred),
-        tf.constant(1, shape=y_pred.shape), tf.constant(0, shape=y_pred.shape))
+    # these two lines set the prediction's max depth value as 1, and every other value as 0
+    comparison = np.equal(np.amax(y_pred, axis=-1, keepdims=True), y_pred)
+    y_pred_oh = np.where(comparison, 1, 0).astype(np.uint8)
 
-    y_true = tf.cast(y_true, dtype=tf.float32)
-    y_pred = tf.cast(y_pred, dtype=tf.float32)
+    intersection = np.multiply(y_true, y_pred_oh)
+    numerator = np.sum(intersection, axis=(0, 1, 2))
+    denominator = np.sum(y_true, axis=(0, 1, 2))
 
-    intersection = tf.math.multiply(y_true, y_pred)
-    numerator = tf.math.reduce_sum(intersection).numpy()
-    denominator = tf.math.cumprod(y_true.shape).numpy()[-2]
+    with np.errstate(all='ignore'):
+        acc_vec = numerator / denominator
 
-    proportion = (numerator / denominator)
-
-    return proportion
+    return acc_vec
