3c3445aa588c619451458216204308d367bf65ca
mpreichenbach <matthew.reichenbach@gmail.com>
2021-09-22 10:37:21 -0600

    Removed tf.math from dice()

--- metrics.py
+++ metrics.py
@@ -4,39 +4,30 @@ def iou(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the IoU-loss for each class,
     # and returns a vector of IoU scores. See https://en.wikipedia.org/wiki/Jaccard_index for more information.
 
-    # y_true = y_true.astype(np.float32)
-    # y_pred = y_pred.astype(np.float32)
-
     intersection = np.multiply(y_true, y_pred)
     i_totals = np.sum(intersection, axis=(0, 1, 2), dtype=np.uint8)
     union = y_true + y_pred - intersection
     u_totals = np.sum(union, axis=(0, 1, 2), dtype=np.uint8)
 
-    # the following division will result in nans, so we suppress the warnings since they're ignored later
-    with np.errstate(divide='ignore'):
+    # the following division will result in nans, so we suppress the warnings since we want to know where nans occur
+    with np.errstate(all='ignore'):
         iou_vec = i_totals / u_totals
 
-    # to ignore than nans in the total, we use np.nansum()
-    scores = np.nansum(iou_vec, axis=0)
-
-    return scores
+    return iou_vec
 
 def dice(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the dice-loss for each class,
     # and returns a vector of dice scores. See https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient for
     # more information. Note that I used Tensorflow math functions to that this may be incorporated into a Keras losses
 
-    y_true = tf.cast(y_true, dtype=tf.float32)
-    y_pred = tf.cast(y_pred, dtype=tf.float32)
+    numerator = 2 * np.sum(np.multiply(y_true, y_pred), axis=(0, 1, 2))
+    denominator = np.sum(y_true, axis=(0, 1, 2)) + np.sum(y_pred, axis=(0, 1, 2))
 
-    numerator = 2 * tf.reduce_sum(tf.math.multiply(y_true, y_pred), axis=(1, 2))
-    denominator = tf.reduce_sum(y_true, axis=(1, 2)) + tf.reduce_sum(y_pred, axis=(1, 2))
+    # the following division can result in nans, so we suppress the warnings since we want to know where nans occur
+    with np.errstate(all='ignore'):
+        dice_vec = numerator / denominator
 
-    # As in iou() above, we don't need to enforce nonzero denominators
-    vec = numerator / denominator
-    scores = tf.reduce_mean(vec, axis=0)
-
-    return scores
+    return dice_vec
 
 def total_acc(y_true, y_pred):
     # Given input tensors of shape (batch_size, height, width, n_classes), this computes the total accuracy of the
