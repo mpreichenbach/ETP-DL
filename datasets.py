f9fb71c767d71698427ec0a946fbb20cb539a3e7
mpreichenbach <matthew.reichenbach@gmail.com>
2022-01-20 17:24:34 -0500

    Updates and typo fixes

--- datasets.py
+++ datasets.py
@@ -4,6 +4,7 @@ import cv2
 from glob import glob
 import tensorflow as tf
 
+
 def load_data(path):
     images = sorted(glob(os.path.join(path, "Train_RGB_256/*")))
     masks = sorted(glob(os.path.join(path, "Train_Labels_256/*")))
@@ -12,27 +13,26 @@ def load_data(path):
 
 def read_image(path):
     x = cv2.imread(path, cv2.IMREAD_COLOR)
-    x /= 255.0
     x = x.astype(np.float32)
+    x /= 255.0
 
     return x
 
 def read_mask(path):
     x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    x /= 255.0
-    x = np.expand_dims(x, axis=-1)
     x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=-1)
 
     return x
 
 def preprocess(x, y):
     def f(x, y):
         # are these lines necessary?
-        # x = x.decode()
-        # y = y.decode()
+        x = x.decode()
+        y = y.decode()
 
         x = read_image(x)
-        y = read_image(y)
+        y = read_mask(y)
 
         return x, y
 
@@ -42,9 +42,9 @@ def preprocess(x, y):
 
     return images, masks
 
-def tf_dataset(x, y, batch=8):
+def tf_dataset(x, y, batch=16):
     dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.shuffle(buff_size=1000)
+    dataset = dataset.shuffle(buffer_size=1000)
     dataset = dataset.map(preprocess)
     dataset = dataset.batch(batch)
     dataset = dataset.prefetch(2)
@@ -54,18 +54,26 @@ def tf_dataset(x, y, batch=8):
 if __name__ == "__main__":
     path = "Data/Potsdam/Numpy Arrays/Train/"
     images, masks = load_data(path)
+    images = images[0:32]
+    masks = masks[0:32]
     print(f"Images: {len(images)} - Masks: {len(masks)}")
 
     dataset = tf_dataset(images, masks)
     counter = 0
     for x, y in dataset:
-        x = x.numpy()
-        y = y.numpy()
+        x = x[0].numpy()
+        y = y[0].numpy()
+        x *= 255.0
+        y *= 255.0
+        x = x.astype(np.uint8)
+        y = y.astype(np.uint8)
+        print(x.shape)
+        print(y.shape)
 
-        cv2.imwrite("Data/Potsdam/Numpy Arrays/test/images/" + str(counter) + ".png")
+        cv2.imwrite("Data/Potsdam/Numpy Arrays/this is a test folder/images/" + str(counter) + ".png", x)
 
         y = np.squeeze(y, axis=-1)
-        cv2.imwrite("Data/Potsdam/Numpy Arrays/test//masks/" + str(counter) + ".png")
+        cv2.imwrite("Data/Potsdam/Numpy Arrays/this is a test folder/masks/" + str(counter) + ".png", y)
         counter += 1
 
         break
