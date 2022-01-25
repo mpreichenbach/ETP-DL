6c76b334d663c181ff8b07a66c900b64e429bb0e
mpreichenbach <matthew.reichenbach@gmail.com>
2022-01-25 16:34:34 -0500

    Finished dataset_gen

--- datasets.py
+++ datasets.py
@@ -1,12 +1,14 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from helper_functions import rotate
+from helper_functions import rotate, label_to_oh
 
 
-def dataset_gen(dim, image_dir, mask_dir, rotate=True, v_flip=True, h_flip=False, seed=1):
+def dataset_gen(dim, classes, batch_size, image_dir, mask_dir, rot8=True, v_flip=True, h_flip=False, seed=1):
     """Creates a pair of iterators which will transform the images/masks in identical ways.
 
     Args:
         dim (int): the width of an image tile,
+        classes (int): the number of labeled classes in the masks,
+        batch_size (int): the number of images to batch together,
         image_dir (str): the path to the folder containing the folder of images,
         mask_dir (str): the path to the folder containing the folder of masks,
         rotate (bool): whether to perform a random 0, 90, 180, or 270 degree rotation,
@@ -14,43 +16,39 @@ def dataset_gen(dim, image_dir, mask_dir, rotate=True, v_flip=True, h_flip=False
         h_flip (bool): whether to perform a flip over the vertical axis (unnecessary when rotation, v_flip=True,
         seed (int): the random seed to set for transformations."""
 
-    if rotate:
-        rot8 = rotate
+    # wraps any preprocessing functions, and performs the one-hot encoding
+    def preprocess(x):
+        x = rotate(x) if rot8 else x
+        x = label_to_oh(x, classes)
+
+        return x
 
     image_datagen = ImageDataGenerator(rescale=1/255.0,
                                        horizontal_flip=h_flip,
                                        vertical_flip=v_flip,
-                                       preprocessing_function=rot8)
+                                       preprocessing_function=preprocess)
 
     mask_datagen = ImageDataGenerator(horizontal_flip=h_flip,
-                                      )
+                                      vertical_flip=v_flip,
+                                      preprocessing_function=preprocess)
 
+    image_generator = image_datagen.flow_from_directory(image_dir,
+                                                        target_size=(dim, dim),
+                                                        class_mode=None,
+                                                        batch_size=batch_size,
+                                                        shuffle=True,
+                                                        save_to_dir='D:/Test/images/',
+                                                        seed=seed)
 
+    mask_generator = mask_datagen.flow_from_directory(mask_dir,
+                                                      target_size=(dim, dim),
+                                                      class_mode=None,
+                                                      batch_size=batch_size,
+                                                      shuffle=True,
+                                                      save_to_dir='D:/Test/masks/',
+                                                      seed=seed)
 
-# we create two instances with the same arguments
-data_gen_args = dict(preprocessing_function=rotate)
-image_datagen = ImageDataGenerator(**data_gen_args)
-mask_datagen = ImageDataGenerator(**data_gen_args)
-# Provide the same seed and keyword arguments to the fit and flow methods
-seed = 1
+    return zip(image_generator, mask_generator)
 
-image_generator = image_datagen.flow_from_directory(
-    'D:/ETP Data/Potsdam/Numpy Arrays/flow from/images/',
-    target_size=(512, 512),
-    class_mode=None,
-    batch_size=1,
-    shuffle=False,
-    save_to_dir='D:/ETP Data/Potsdam/Numpy Arrays/flow to/images/',
-    seed=seed)
-mask_generator = mask_datagen.flow_from_directory(
-    'D:/ETP Data/Potsdam/Numpy Arrays/flow from/masks/',
-    target_size=(512, 512),
-    class_mode=None,
-    batch_size=1,
-    shuffle=False,
-    save_to_dir='D:/ETP Data/Potsdam/Numpy Arrays/flow to/masks/',
-    seed=seed)
-# combine generators into one which yields image and masks
-train_generator = zip(image_generator, mask_generator)
 
 
