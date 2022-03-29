4019beea1dcd7c2fcacb79bdabd91a910dbe4b57
mpreichenbach <matthew.reichenbach@gmail.com>
2022-03-29 17:11:33 -0400

    Added log_iou to losses.py.

--- losses.py
+++ losses.py
@@ -2,7 +2,10 @@ from tensorflow.keras import backend as K
 
 
 def iou_loss(y_true, y_pred):
-    """Computes the loss 1 - IoU; note that this function as written only works on binary data.
+    """Computes a variation of the loss 1 - IoU, the difference being the inclusion of the 'smooth' parameter to ensure
+     no division by zero, and a scaling parameter to ensure IoU scores in the range [0, 1]. Note that this function as
+     written only works on binary data.
+
     Args:
         y_true: tensor of ground-truth values of size (batch, height, width), so not one-hot encoded;
         y_pred: tensor of model predictions of size (batch, height, width, 2), so one-hot encoded."""
@@ -10,10 +13,26 @@ def iou_loss(y_true, y_pred):
     # ensures no division by zero, which can occur when a model accurately predicts no instances of the class.
     smooth = 1
 
-    intersection = K.cumsum(K.flatten(y_true * y_pred[-1]))
+    intersection = 2 * K.cumsum(K.flatten(y_true * y_pred[-1]))
     union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred[-1])) + smooth
 
+    # perfect overlap implies iou = 1 due to the smooth parameter value, perfect non-overlap implies  iou = 0.
     iou = intersection / union
 
     return 1 - iou
 
+def log_iou(y_true, y_pred):
+    """Compute a variation of the -log(IoU) loss introduced in 'Unitbox': An Advanced Object Detection Network. This
+    version includes the 'smooth' parameter to ensure no division by zero.
+
+    Args:
+        y_true: tensor of ground-truth values of size (batch, height, width), so not one-hot encoded;
+        y_pred: tensor of model predictions of size (batch, height, width, 2), so one-hot encoded."""
+    
+    # ensures a nonzero numerator and denominator, so that the logarithm is well-define.
+    smooth = 1
+
+    intersection = K.cumsum(K.flatten(y_true * y_pred[-1])) + smooth
+    union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred[-1])) + smooth
+
+    return - K.log(intersection / union)
