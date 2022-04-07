aa3b0b64370c7742b845ccefbf081f327825eb61
mpreichenbach <matthew.reichenbach@gmail.com>
2022-04-07 17:27:42 -0400

    Slight fixes to losses.

--- losses.py
+++ losses.py
@@ -3,11 +3,10 @@ from tensorflow.keras import backend as K
 
 def iou_loss(y_true, y_pred):
     """Computes a variation of the loss 1 - IoU, the difference being the inclusion of the 'smooth' parameter to ensure
-     no division by zero, and a scaling parameter to ensure IoU scores in the range [0, 1]. Note that this function as
-     written only works on binary data.
+     no division by zero, and a scaling parameter to ensure IoU scores in the range [0, 1].
 
     Args:
-        y_true: tensor of ground-truth values of size (batch, height, width), so not one-hot encoded;
+        y_true: tensor of ground-truth values of size (batch, height, width, 2);
         y_pred: tensor of model predictions of size (batch, height, width, 2), so one-hot encoded."""
 
     # ensures no division by zero, which can occur when a model accurately predicts no instances of the class.
@@ -32,7 +31,7 @@ def log_iou_loss(y_true, y_pred):
     # ensures a nonzero numerator and denominator, so that the logarithm is well-define.
     smooth = 1
 
-    intersection = 2 * K.sum(K.flatten(y_true * y_pred)) + smooth
+    intersection = 2 * K.sum(K.flatten(y_true * y_pred))
     union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred)) + smooth
 
     return - K.log(intersection / union)
