97c933e83e2322bd41127cb8d3d5017f7b9bc676
mpreichenbach <matthew.reichenbach@gmail.com>
2022-06-28 12:12:18 -0400

    Added padding/prediction to updatePixels method.

--- ArcGIS Tool/BuildingClassifier.py
+++ ArcGIS Tool/BuildingClassifier.py
@@ -35,7 +35,7 @@ class BuildingClassifier:
                 'dataType': 'string',
                 'required': True,
                 'displayName': "Input Model Description (EMD) File",
-                'description': "INput model description (EMD) JSON file."
+                'description': "Input model description (EMD) JSON file."
             }
         ]
 
@@ -81,17 +81,51 @@ class BuildingClassifier:
 
     # this is the workhorse method for the Classify Pixels Using Deep Learning tool
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
-        # obtain the input image
+
+        # this function converts one-hot encoded pixels to integer labels
+        def vec_to_label(oh_array):
+            output = np.argmax(oh_array, axis=-1).astype(np.uint8)
+
+            return output
+
+        # obtain the input image, initialize prediction holder
         input_image = pixelBlocks['raster_pixels']
 
-        # Todo: transform the input image to the shape of the model's input
-        # input_image = np.transform(input_image, ....)
+        # dimensions of inference window (square) for the neural network
+        d = 1024
 
-        # Todo: run the model on the transformed input image, something like
-        # model_output = self.model.run(input_image)
+        # compute appropriate padding amounts for the prediction holder
+        output_padding = [0, 0]
+        input_dims = input_image.shape[1:3]
+        for i in range(2):
+            if input_dims[i] % d == 0:
+                continue
+            else:
+                b = input_dims[i]
+                output_padding[i] = int(0.5 * ((d * (int(b / d) + 1)) - b))
 
-        # Todo: wrap the classified raster in dictonary, something like
-        # pixelBlocks['output_pixels'] =  model_output.astype(props['pixelType'], copy=False)
+        # initialize the prediction holder and pad the input
+        ph, pw = output_padding
+        padded_input = np.pad(input_image, ((0,), (ph,), (pw,)), mode='reflect')
+        padded_predictions = np.zeros(padded_input.shape, dtype=np.uint8)
 
-        # Return the dict
+        # run inference on tiles of padded_input, place into prediction_holder
+        n_tiles_height = int(padded_input / d)
+        n_tiles_width = int(padded_input / d)
+        padded_input = np.moveaxis(padded_input, 0, 2)
+
+        for i in range(n_tiles_height):
+            for j in range(n_tiles_width):
+                input_tile = np.expand_dims(padded_input[(d * i):(d * (i + 1)), (d + j):(d * (j + 1)), ], 0)
+                predicted_tile = np.squeeze(self.model.predict(input_tile))
+                predicted_tile = vec_to_label(predicted_tile)
+
+                padded_predictions[(d * i):(d * (i + 1)), (d + j):(d * (j + 1))] = predicted_tile
+
+        # keep only the dimensions of the original input
+        unpadded_predictions = padded_predictions[ph:-ph, pw:-pw]
+
+        # put the output into the dictionary, and return it
+        pixelBlocks['output_pixels'] = unpadded_predictions
+
         return pixelBlocks
